35169b1f6ea80d03570f0244e1a7125d1b7d78f1
YeHIT <1044377312@qq.com>
2020-12-24 17:53:45 -0800

    2020-12-25 修改了shutdown处理 --- Ye

--- fileSystem/main.c
+++ fileSystem/main.c
@@ -27,23 +27,23 @@ int main(int argc, char const *argv[]){
 	static char buf[MAX_COMMAND_LENGTH];
     //读取指令直到结束
 	while(getcmd(buf,sizeof(buf)) >= 0){
-        //子进程进行
-		if(myFork() == 0){
-            //参数列表
-			char* argv[MAXARGS];
-            //参数数量
-			int argc = 0;
-			AnalyseCmd(buf,argv,&argc);
-            if(strcmp(argv[0],"shutdown") == 0){
-                if(argc != 1){
-                    printf("shutdown argument error\n");
-                    return 0;
-                }
-                printf("thank you for using Ye's file system!!\n");
-                kill(main_proc,SIGKILL);
-                return 0;
+        //参数列表
+        char* argv[MAXARGS];
+        //参数数量
+        int argc = 0;
+        AnalyseCmd(buf,argv,&argc);
+        if(strcmp(argv[0],"shutdown") == 0){
+            if(argc != 1){
+                printf("shutdown argument error\n");
             }
             else{
+                printf("thank you for using Ye's file system!!\n");
+                return 0;
+            }
+        }
+        //子进程进行
+		if(myFork() == 0){
+            if(strcmp(argv[0],"shutdown") != 0){
         	    RunCmd(argc,argv);
             }
 		}
